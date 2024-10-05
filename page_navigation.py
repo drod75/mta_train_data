3a4134c3f0c2bca3ca7e303d8cde6281c8b1a50e
drod75 <dr507498@gmail.com>
2024-10-05 19:08:53 -0400

    edits

--- page_navigation.py
+++ page_navigation.py
@@ -1,4 +1,12 @@
 import streamlit as st
-pages = {}
+
+with open( "app/styles/fonts.css" ) as css:
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html=True)
+pages = [
+    st.Page('streamlit_app/home.py',title='MTA Base', icon=':material/domain:', default=True),
+    st.Page('streamlit_app/mta_log.py',title='MTA Train Log', icon=':material/subway:'),
+    st.Page('streamlit_app/mta_analysis.py',title='MTA Ridership', icon=':material/groups:'),
+    st.Page('streamlit_app/mta_ai.py',title='MT-AI', icon=':material/robot_2:')
+]
 pg = st.navigation(pages)
 pg.run()
