6e8dcce0f7d8b38aec064384b9ef01f97ccb7d76
drod75 <dr507498@gmail.com>
2024-10-11 18:45:29 -0400

    edits

--- page_navigation.py
+++ page_navigation.py
@@ -4,8 +4,8 @@ with open( "app/styles/style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html=True)
 pages = [
     st.Page('streamlit_app/home.py',title='MTA Base', icon=':material/domain:', default=True),
-    st.Page('streamlit_app/mta_suggestions.py',title='MTA Line Suggestion', icon=':material/subway:'),
     st.Page('streamlit_app/mta_analysis.py',title='MTA Ridership', icon=':material/groups:'),
+    st.Page('streamlit_app/mta_suggestions.py',title='MTA Line Suggestion', icon=':material/subway:'),
     st.Page('streamlit_app/mta_ai.py',title='MT-AI', icon=':material/robot_2:')
 ]
 pg = st.navigation(pages)
--- plots_st.py
+++ plots_st.py
@@ -1,12 +1,38 @@
 import plotly.express as px
 import pandas as pd
+import streamlit as st
+import streamlit_folium as st_folium
+import folium
+from folium.plugins import HeatMap
 
 def plots_for_st(df: pd.DataFrame):
-    fig1 = px.bar(df, x='BOROUGH', y='SUM_RIDERSHIP', color='BOROUGH', title='Ridership by Stations')
-    
-    fig2 = px.treemap(df[df['BOROUGH'] == 'Manhattan'],values='SUM_RIDERSHIP', names='STATION', parents='BOROUGH',title='Manhattan Treemap')
-    fig3 = px.treemap(df[df['BOROUGH'] == 'Bronx'],values='SUM_RIDERSHIP', names='STATION', parents='BOROUGH', title='Bronx Treemap')
-    fig4 = px.treemap(df[df['BOROUGH'] == 'Brooklyn'],values='SUM_RIDERSHIP', names='STATION', parents='BOROUGH', title='Brooklyn Treemap') 
-    fig5 = px.treemap(df[df['BOROUGH'] == 'Queens'],values='SUM_RIDERSHIP', names='STATION', parents='BOROUGH', title='Queens Treemap')
- 
-    return [fig1,fig2,fig3,fig4,fig5]
+    option = st.selectbox('What Borough would you like to analyze?', ['Manhattan', 'Bronx', 'Brooklyn', 'Queens', 'ALL'], index=None, placeholder='Select a Borough')
+    figure_list = []
+    match option:
+        case 'ALL':
+            pass
+        case 'Manhattan':
+            df_manhattan = df[df['BOROUGH'] == 'Manhattan']
+            ridership = df_manhattan['SUM_RIDERSHIP']
+            m = folium.Map(location=[40.779393,-73.966847], zoom_start=12)
+            HeatMap(list(zip(df_manhattan['LATITUDE'], df_manhattan['LONGITUDE'], ridership)), radius=10).add_to(m)
+            st_folium.folium_static(m)
+        case 'Bronx':
+            df_bronx = df[df['BOROUGH'] == 'Bronx']
+            ridership = df_bronx['SUM_RIDERSHIP']
+            m = folium.Map(location=[40.845881,-73.876357], zoom_start=12)
+            HeatMap(list(zip(df_bronx['LATITUDE'], df_bronx['LONGITUDE'], ridership)), radius=10).add_to(m)
+            st_folium.folium_static(m)
+        case 'Brooklyn':
+            df_brooklyn = df[df['BOROUGH'] == 'Brooklyn']
+            ridership = df_brooklyn['SUM_RIDERSHIP']
+            m = folium.Map(location=[40.646641,-73.955603], zoom_start=12)
+            HeatMap(list(zip(df_brooklyn['LATITUDE'], df_brooklyn['LONGITUDE'], ridership)), radius=10).add_to(m)
+            st_folium.folium_static(m)  
+        case 'Queens':
+            df_queens = df[df['BOROUGH'] == 'Queens']
+            ridership = df_queens['SUM_RIDERSHIP']
+            m = folium.Map(location=[40.711386,-73.827573], zoom_start=12)
+            HeatMap(list(zip(df_queens['LATITUDE'], df_queens['LONGITUDE'], ridership)), radius=10).add_to(m)
+            st_folium.folium_static(m)
+    return figure_list
