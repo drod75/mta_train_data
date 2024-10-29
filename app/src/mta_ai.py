df484d6c0ad5cc4860f02acd01a178203bbb4d55
drod75 <dr507498@gmail.com>
2024-10-29 12:03:54 -0400

    'ai edits'

--- app/src/mta_ai.py
+++ app/src/mta_ai.py
@@ -4,6 +4,17 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
+# model loading
+load_dotenv(Path(".env"))
+model = ChatGoogleGenerativeAI(
+        model="gemini-1.5-pro",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        api_key=os.environ.get('GEMINI_API_KEY')
+)
+
 
 with open( "app/styles/style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html=True)
@@ -12,16 +23,8 @@ st.header('MT-AI')
 st.markdown('### This is the MT-AI page!')
 
 def generate_response(input_text):
-    load_dotenv(Path(".env"))
-    model = ChatGoogleGenerativeAI(
-        model="gemini-1.5-pro",
-        temperature=0,
-        max_tokens=None,
-        timeout=None,
-        max_retries=2,
-        api_key=os.environ.get('GEMINI_API_KEY')
-    )
-    st.info(model.invoke(input_text))
+    response = model.invoke(input_text)
+    st.info(response)
 
 
 with st.form("my_form"):
