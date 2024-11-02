c93b5ae0c2d8564ada7132ae929ac743955e88e0
drod75 <dr507498@gmail.com>
2024-11-02 15:01:53 -0400

    req and analysis edits

--- app/src/mta_analysis.py
+++ app/src/mta_analysis.py
@@ -32,22 +32,21 @@ match chosen_id:
     # Tab two data viewing
     case '2':
         option = st.selectbox('What Borough would you like to see?', ('Brooklyn', 'Manhattan',
-                'Queens', 'Bronx', 'ALL'), index=None, placeholder='Please Select a choice...')
-        if option =='ALL':
+                                                                      'Queens', 'Bronx', 'ALL'), index=None, placeholder='Please Select a choice...')
+        if option == 'ALL':
             option2 = st.selectbox('The table comes with 485 entries, would you like to see data from the bottom or top?', (
-                    'Top', 'Bottom'), index=None, placeholder='Please Select a choice...')
+                'Top', 'Bottom'), index=None, placeholder='Please Select a choice...')
             option3 = st.slider('Pick a number', 0, 485)
             # Option 2, top or bottom of all, and how many
-            while option2 != None:
-                match option2:
-                    case 'Top':
-                        slc = df.head(option3)
-                        slc = slc.sort_values('SUM_RIDERSHIP', ascending=False)
-                        st.table(slc)
-                    case 'Bottom':
-                        slc = df.tail(option3)
-                        slc = slc.sort_values('SUM_RIDERSHIP', ascending=False)
-                        st.table(slc)
+            match option2:
+                case 'Top':
+                    slc = df.head(option3)
+                    slc = slc.sort_values('SUM_RIDERSHIP', ascending=False)
+                    st.table(slc)
+                case 'Bottom':
+                    slc = df.tail(option3)
+                    slc = slc.sort_values('SUM_RIDERSHIP', ascending=False)
+                    st.table(slc)
         else:
             slc = df[df['BOROUGH'] == option]
             slc = slc.sort_values('SUM_RIDERSHIP', ascending=False)
